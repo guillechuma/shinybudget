3b4066b3cd65c3e456442a1564dfceb6947732ef
Guillermo Chumaceiro <guillechuma24@gmail.com>
2024-08-20 01:34:05 +0200

    Added themes

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -1,9 +1,11 @@
 from shiny import ui
 from ui.expenses_ui import expenses_ui
+import shinyswatch
 
 def main_ui():
     return ui.page_fluid(
         ui.navset_tab(
             ui.nav_panel("Expenses", expenses_ui())
-        )
+        ),
+        theme=shinyswatch.theme.vapor,
     )
